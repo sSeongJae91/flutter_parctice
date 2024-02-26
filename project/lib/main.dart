bd00f268fabb4b1d7b5140647e241f549afbfde7
rockettttman <ben91429@gmail.com>
2024-02-26 16:39:12 +0900

    row, column, media query

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -47,28 +47,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test Title"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text("Drawer Hader Part"),
-            ),
-            ListTile(
-              title: Text("Menu 1"),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text("Test Title"),
         ),
-      ),
-      body: const Center(child: Text("Hello World")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("clicked"),
-        child: const Icon(Icons.mouse),
-      ),
-    );
+        body: Container(
+          alignment: Alignment.topLeft,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 200,
+                      color: Colors.blue),
+                  Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 200,
+                      color: Colors.red)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 200,
+                      color: Colors.green),
+                  Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 200,
+                      color: Colors.purple)
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
